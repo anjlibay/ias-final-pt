53c269662ea183d21b356be906d3b110deef12cd
anjlibay <libay.angela01@gmail.com>
2024-05-24 22:56:49 +0800

    my final commit

--- src/pages/firebase.tsx
+++ src/pages/firebase.tsx
@@ -7,13 +7,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjT0TTWyhc7y8LTIZaWRkUJshC0tRPYeE",
-  authDomain: "final-pt-f3af7.firebaseapp.com",
-  projectId: "final-pt-f3af7",
-  storageBucket: "final-pt-f3af7.appspot.com",
-  messagingSenderId: "1010079629577",
-  appId: "1:1010079629577:web:ca70877e6bc5c2bb5c53d8",
-  measurementId: "G-WPHMHXCSKW"
+  apiKey: "AIzaSyCKu6EzykzzOo2GFmXqQDFFMgrDTUJBvvc",
+  authDomain: "final-pt-d0d28.firebaseapp.com",
+  projectId: "final-pt-d0d28",
+  storageBucket: "final-pt-d0d28.appspot.com",
+  messagingSenderId: "980227001920",
+  appId: "1:980227001920:web:41296ae6d93c6bf72799a5",
+  measurementId: "G-FCFQ1DDRWF"
 };
 
 // Initialize Firebase
